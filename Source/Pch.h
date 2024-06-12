d7ea06950a3a719ef77a689148983838c94d2b2d
Lorenzo Raineri <lorenzorai05@gmail.com>
2024-06-12 20:32:29 +0200

    Moved files into Conversion style layout

--- Source/Pch.h
+++ Source/Pch.h
@@ -27,8 +27,10 @@
 #include <algorithm>	
 #include <stdio.h>
 
-// Other
+// HE1ML
 #include <ModLoader.h>
+
+// Other
 #include <INIReader.h>
 #include <json/json.h>
 #include <Helpers.h>
@@ -36,24 +38,24 @@
 #include <StringHelper.h>
 #include <rapidxml/rapidxml.hpp>
 #include <winuser.h>
-
 #include <HavokEssentials.h>
 #include "ScoreGenerationsAPI.h"
-
-
 #define DEBUG_DRAW_TEXT_DLL_IMPORT	
-#include "XMLParser.h"
 #include "DebugDrawText.h"	
-#include "Time.h"
-#include "Temporary.h"	
-#include "WerehogStates.h"
-#include "Testing.h"	
-#include "EnemyBase.h"	
-#include "CustomAnimationManager.h"	
-#include "ClassicPluginExtensions.h"	
 
-#include "ConversionAPI.h"
-#include "ArchiveTreePatcher.h"
+//System
 #include "Configuration.h"
+#include "System\Time.h"
+#include "System\Temporary.h"	
+#include "System\Testing.h"	
+#include "System\CustomAnimationManager.h"	
+#include "System\ClassicPluginExtensions.h"	
+#include "System\ConversionAPI.h"
+#include "System\XMLParser.h"
+#include "System\ArchiveTreePatcher.h"
+
+//EvilStates
+#include "WerehogStates.h"
+#include "EnemyBase.h"	
 #include "CStateArmSwing.h"
 #include "EvilSonic.h"
